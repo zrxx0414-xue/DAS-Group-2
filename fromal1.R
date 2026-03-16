c80371959713a083f82278ffbd33dae4097918d8
RuixueZhao <zrxx0414@gmail.com>
2026-03-16 23:35:54 +0000

    Add files via upload

--- fromal1.R
+++ fromal1.R
@@ -1,11 +1,12 @@
 df=read.csv("C:/Users/86185/Downloads/DAProject2.csv",stringsAsFactors = T)
-#question1
+##question1
 fit_year <- lm(BMI ~ factor(Year), data = df)
 summary(fit_year)
 anova(fit_year)
 #no changes for BMI over years
 
-#question2
+
+##question2
 #smooth curve from EDA showed a non-linear relationship btw BMI and Age, so quadratic age term was added to the model.
 fit_age=lm(BMI ~ Age + I(Age^2),data=df)
 summary(fit_age)
@@ -33,7 +34,6 @@ ggplot(df, aes(x = Fruit, fill = Veg)) +
   theme_minimal()
 
 
-
 fit_quad_2   <- lm(BMI ~ Age + I(Age^2) +  Education + Veg + Fruit, data = df)
 summary(fit_quad_2)
 AIC(fit_quad_2)
@@ -43,6 +43,26 @@ summary(fit_quad_3)
 AIC(fit_quad_2)
 #model is not improved by dropping fruit
 
-#we can plot a comparsion for fit_quad_1&fit_quad_2&fit_quad_3
+#comparison
+model_comp <- data.frame(
+  Model = c("fit_quad_1", "fit_quad_2", "fit_quad_3"),
+  Adj_R2 = c(
+    summary(fit_quad_1)$adj.r.squared,
+    summary(fit_quad_2)$adj.r.squared,
+    summary(fit_quad_3)$adj.r.squared
+  ),
+  AIC = c(
+    AIC(fit_quad_1),
+    AIC(fit_quad_2),
+    AIC(fit_quad_3)
+  ),
+  BIC = c(
+    BIC(fit_quad_1),
+    BIC(fit_quad_2),
+    BIC(fit_quad_3)
+  )
+)
+
+model_comp
 #where we can conclude fit_quad_2 is the best fit
 #i.e. BMI is relative with age,edu,veg,fruit--but not--sex,year
