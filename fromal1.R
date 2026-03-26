6382fbe6c664b0e84bb334484671097825f4cc6e
RuixueZhao <zrxx0414@gmail.com>
2026-03-26 13:52:07 +0000

    Refactor BMI analysis in fromal1.R
    Refactor R script for BMI analysis, adding libraries and updating model diagnostics. Include visualizations and conclusions based on data trends.

--- fromal1.R
+++ fromal1.R
@@ -1,68 +1,64 @@
-df=read.csv("DAProject2.csv",stringsAsFactors = T)
-##question1
-fit_year <- lm(BMI ~ factor(Year), data = df)
-summary(fit_year)
-anova(fit_year)
-#no changes for BMI over years
+------------------------------------------------------------------------
+
+**Question 1**
+
+```{r}
+library(tidyverse)
+library(ggplot2)
+library(performance)
+library(see)
+library(sjPlot)
+library(kableExtra)
+library(broom)
+library(knitr)
+library(car)
+```
+
+```{r}
+#| eval: false
+mean <- dataset%>%
+  summarise(mean_BMI = mean(BMI), 
+            .by = Year)
+ggplot(mean, aes(x = Year, y = mean_BMI))+
+  geom_line()
+```
+
+From the plot the it is suggested that there is no significant trend followed by the observation over 2008-2012
+
+```{r}
+#The Model for question 1 
+time_model <- lm(BMI~Year, data = dataset)
+summary(time_model)
+#we will chech the model diagnostics 
+check_model(time_model)
 
 
-##question2
-#smooth curve from EDA showed a non-linear relationship btw BMI and Age, so quadratic age term was added to the model.
-fit_age=lm(BMI ~ Age + I(Age^2),data=df)
-summary(fit_age)
-#including Age^2 is significant
-fit_linear <- lm(BMI ~ Age + Sex + Education + Veg + Fruit, data = df)
-fit_quad_1   <- lm(BMI ~ Age + I(Age^2) + Sex + Education + Veg+ Fruit, data = df)
-anova(fit_linear, fit_quad_1)
-summary(fit_quad_1)
-AIC(fit_quad_1)
-#where sex and fruit are not significant
-
-fit_sex=lm(BMI ~ Sex,data=df)
-summary(fit_sex)
-#sex not significant by itself
-fit_fruit=lm(BMI ~ Fruit,data=df)
-summary(fit_fruit)
-#fruit significant by itself
-#cuz there might be strong correlation btw Veg and Fruit
-
-#EDA (Veg~Fruit)
-chisq.test(table(df$Fruit, df$Veg))
-ggplot(df, aes(x = Fruit, fill = Veg)) +
-  geom_bar(position = "fill") +
-  labs(y = "Proportion") +
-  theme_minimal()
+#complementary model and its check no need to include it 
+#model_q1 <-lm(formula = BMI ~ Age + I(Age^2) + Education +
+    #Veg + Year + Age:Sex + Sex:Education + Fruit:Education, data = dataset)
+#Note that I have added the year term in the final model becuase we are checking prevelance with time. 
+#summary(model_q1)
+#check_model(model_q1)
+#check_model(model_q1,check = c("linearity","homogeneity"))
+#check_model(model_q1,check = c("qq","normality"))
 
 
-fit_quad_2   <- lm(BMI ~ Age + I(Age^2) +  Education + Veg + Fruit, data = df)
-summary(fit_quad_2)
-AIC(fit_quad_2)
-#model is improved by dropping sex
-fit_quad_3   <- lm(BMI ~ Age + I(Age^2) +  Education + Veg, data = df)
-summary(fit_quad_3)
-AIC(fit_quad_2)
-#model is not improved by dropping fruit
+```
 
-#comparison
-model_comp <- data.frame(
-  Model = c("fit_quad_1", "fit_quad_2", "fit_quad_3"),
-  Adj_R2 = c(
-    summary(fit_quad_1)$adj.r.squared,
-    summary(fit_quad_2)$adj.r.squared,
-    summary(fit_quad_3)$adj.r.squared
-  ),
-  AIC = c(
-    AIC(fit_quad_1),
-    AIC(fit_quad_2),
-    AIC(fit_quad_3)
-  ),
-  BIC = c(
-    BIC(fit_quad_1),
-    BIC(fit_quad_2),
-    BIC(fit_quad_3)
-  )
-)
+```{r}
+tidy(time_model, conf.int = TRUE) %>%
+  select(term, estimate, p.value, conf.low, conf.high) %>%
+  kable(
+    digits = 3, 
+    booktabs = TRUE, 
+    col.names = c("Term", "Estimate", "P-Value", "Lower CI", "Upper CI") ) %>%
+  kable_styling(latex_options = c("striped", "hold_position", "scale_down"), full_width = FALSE, font_size = 8) %>%
+  # Adjust column 1 (Predictors) to be 4cm wide
+  column_spec(1, width = "4cm") %>%
+  # Adjust the rest to be narrower (e.g., 2cm)
+  column_spec(2:5, width = "2cm")
+```
 
-model_comp
-#where we can conclude fit_quad_2 is the best fit
-#i.e. BMI is relative with age,edu,veg,fruit--but not--sex,year
+Here year is not a significant predictor of BMI as the p-value is greater than 0.05, however we may still see an increase in BMI over the years due to the coefficient term being positive. The confidence interval contains 0, further suggesting that there is no prevalence in BMI from year to another.
+
+In conclusion there have been changes in BMI over time as seen from the plot, but changes in year is not what is causing the change in BMI. Thus, variation in BMI is explained by peoples education and lifestyle status rather than the year when data was collected.
